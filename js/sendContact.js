22655e64566976bbef452196b5c226b2794a8505
Kevin Jimenez <kevinalexisjimenezguerrero@gmail.com>
2022-07-08 16:54:09 -0500

    feat: titulos y logo en las paginas, validacion del captcha

--- js/sendContact.js
+++ js/sendContact.js
@@ -4,7 +4,7 @@ async function send(){
     apellidos = document.getElementById('apellidos');
     correo = document.getElementById('correo');
     telefono = document.getElementById('telefono');
-
+    var response = grecaptcha.getResponse();
     if(nombre.value == null || nombre.value == '' || nombre.value.length == 0){
         nombre.classList.add('requerido');       
     } 
@@ -17,6 +17,9 @@ async function send(){
     if(telefono.value == null || telefono.value == '' || telefono.value.length == 0){
         telefono.classList.add('requerido');       
     }
+    if(response.length == 0){
+        alert("Captcha no verificado")
+    } 
     const rawResponse = await fetch('https://dood-azell-giveaway-api.herokuapp.com/urbk-us', {
         method: 'POST',
         headers: {
@@ -24,8 +27,30 @@ async function send(){
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({name: nombre.value+' '+apellidos.value, email: correo.value, phoneNumber: telefono.value})
-      });
+      });      
       const content = await rawResponse.json();
-
-      console.log(content);
+      let cont = document.getElementById('cont');
+      if(rawResponse.status == 200){
+        nombre.value = "";
+        apellidos.value = "";
+        correo.value = "";
+        telefono.value = "";
+        alert('Tus datos han sido enviados correctamente');
+        // cont.insertAdjacentHTML(
+        //     `<div class="alert alert-success alert-dismissible fade show" role="alert">
+        //         Tus datos han sido enviados correctamente.
+        //         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        //     </div>`
+        // )
+      }else{
+        alert('Error '+ content.message)
+        // cont.insertAdjacentHTML(
+        //     `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        //         <strong>Error!</strong>${content.message}.
+        //         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        //     </div>`
+        // )
+         
+      }
+    //   console.log(rawResponse.status);
 }
